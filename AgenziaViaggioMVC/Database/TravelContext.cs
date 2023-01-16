0b1b7fa07a8aab9a9f99939fa53148925b8465dd
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-16 14:30:44 +0100

    Creazione del DbContext. Correzione e validazione del modello Travel

---
+++ AgenziaViaggioMVC/Database/TravelContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AgenziaViaggioMVC.Models;
+
+namespace AgenziaViaggioMVC.Database
+{
+    public class TravelContext : DbContext
+    {
+        public DbSet<Travel> Travels { get;set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = localhost; Database = TravelAgencyV1; " + "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
