def1e45d154fb44692620389988d796fe56ed6f8
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-16 12:52:18 +0100

    Prima stesura del modello Travel.

---
+++ AgenziaViaggioMVC/Models/Travel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgenziaViaggioMVC.Models
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(500)")]
+        public string? ImageUrl { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string? Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "smallint")]
+        public string? Days { get; set; }
+
+        [Column(TypeName = "smallint")]
+        public string? Destinations { get; set; }
+
+        [Column(TypeName = "int")]
+        public string? Price { get; set; }
+    }
+}
